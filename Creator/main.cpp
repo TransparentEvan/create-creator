0ffb1237713cfac7f3b001f80cb70deee59038c7
TransparentEvan <562854509@qq.com>
2015-06-23 21:24:17 +0800

    dsgfe

--- Creator/main.cpp
+++ Creator/main.cpp
@@ -1,6 +1,41 @@
 #include<stdio.h>
+struct chapter
+{
+	char _chaptername[100];
+	char question[10][100];	//第一维为问题编号，第二维为问题内容
+	char _answer[10][100];//与问题对应 
+};
+struct subject
+{
+	char _subjectname[100];
+	struct chapter section[10];
+};
+struct homework
+{
+	subject project[3];
+};
+
+struct subject computerscience[10];
+
+/*<subject1 name = "math">
+	<chapter1>
+		<name> 函数 </name>
+		<question1> 下列哪些是函数 </questong1>
+		<answer> 都不是函数 </answer>
+	</chapter1>
+</subject1>
+<subject2 name = "c language">
+	<chapter1>
+		<name> 变量 </name>
+		<question1> 下列那些变量声明是合法的 </question1>
+		<answer> 都是合法的 </answer>
+	</chapter1>
+</subject2>*/
+	
 int main()
 {
-
+	FILE *fp;
+	char filename[20], tmp_store[100];
+	fopen
 	return 0;
 }
